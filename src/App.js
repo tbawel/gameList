e3ff05b3018d723839ffee729d1384cf1b7225fd
Tom Bawel <tbawel@gmail.com>
2019-05-09 18:29:23 -0600

    edit rating

--- src/App.js
+++ src/App.js
@@ -35,27 +35,84 @@ class App extends React.Component {
       ],
       newGame: {
         name: "",
-        rating: 0
+        rating: ""
+      },
+      editGame: {
+        name: "",
+        rating: ""
       }
     }
-
-
   };
 
-  handleClick = (e, gameID) => {
-    let newGames = this.state.games.filter((game) => (game.id !== gameID));
+handleEdit = (e, gameId) => {
+  let newGames = this.state.games.map(game => {
+    if (game.id === gameId) {
+      this.setState({editGame: {
+        name: game.name,
+        rating: game.rating
+      }
+    })
+      return {
+        ...game,
+        edit: !game.edit
+      };
+    } else {
+      return {
+        ...game,
+        edit: false
+      };
+    }
+  });
+  this.setState({
+    games: newGames
+  });
+};
+
+  handleDelete = (e, gameId) => {
+    let newGames = this.state.games.filter((game) => (game.id !== gameId));
     this.setState({ games: newGames });
   }
 
-  handleChange = e => {
+  handleNewChange = e => {
     this.setState({
       newGame: {
         ...this.state.newGame,
         [e.target.name]: e.target.value
       }
-    })
-  }
-//added comment
+    });
+  };
+
+  handleEditChange = e => {
+    this.setState({
+      editGame: {
+        ...this.state.editGame,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  handleUpdate = (e, gameId) => {
+    e.preventDefault();
+    let editGames = this.state.games.map(game => {
+      if(game.id === gameId) {
+        return {
+          ...game,
+          name: this.state.editGame.name,
+          rating: this.state.editGame.rating,
+          edit: false
+        };
+      } else {
+        return {
+          ...game
+        };
+      }
+    });
+    this.setState({
+      games: editGames
+    });
+
+  };
+
   addGame = (e) => {
     e.preventDefault();
     let tempList = this.state.games.slice();
@@ -67,7 +124,29 @@ class App extends React.Component {
   render() {
     const gamesList = this.state.games.map((game) =>
       <li key={game.id}>{game.name} has a rating of {game.rating}/10
-      <button onClick={(e) => this.handleClick(e, game.id)}> Delete </button>
+      <button onClick={(e) => this.handleEdit(e, game.id)}> Edit </button>
+      <button onClick={(e) => this.handleDelete(e, game.id)}> Delete </button>
+      <br />
+      {game.edit ? (
+        <form onSubmit={e => this.handleUpdate(e, game.id)}>
+          <input
+            type="text"
+            value={this.state.editGame.name}
+            placeholder={game.name}
+            name="name"
+            onChange={e => this.handleEditChange(e)}/>
+          <input
+            type="number"
+            value={this.state.editGame.rating}
+            placeholder={game.rating}
+            name="rating"
+            onChange={e => this.handleEditChange(e)}/>
+          <button type="submit">Update</button>
+        </form>
+      ) : (
+        ""
+      )
+      }
       </li>
     );
 
@@ -80,14 +159,15 @@ class App extends React.Component {
             name="name"
             placeholder="Type new game here"
             value={this.state.newGame.name}
-            onChange={(e) => this.handleChange(e)} />
+            onChange={(e) => this.handleNewChange(e)} />
           <label>  Rating: </label>
           <input type="number"
             name="rating"
             min="0"
             max="10"
+            placeholder="Enter Value"
             value={this.state.newGame.rating}
-            onChange={(e) => this.handleChange(e)} />
+            onChange={(e) => this.handleNewChange(e)} />
           <button type="submit">Add</button>
         </form>
 
@@ -97,3 +177,4 @@ class App extends React.Component {
 }
 
 export default App;
+
